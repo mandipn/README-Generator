776f3003a813ac52d1b4077fa71868eb9ae6f9c1
Mandip Nijor <mandip.nijor@gmail.com>
2023-01-20 18:47:01 +0000

    prompt questions added

--- starter/index.js
+++ starter/index.js
@@ -6,6 +6,41 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [
 
+    .prompt([
+
+        {
+          type: 'input',
+          message: 'Please enter the title of your project',
+          name: 'title',
+          },
+        {
+          type: 'input',
+          message: 'Please describe your project',
+          name: 'describe',
+        },
+        {
+          type: 'input',
+          message: 'Please enter the installation instructions for your project',
+          name: 'install',
+        },
+        {
+          type: 'input',
+          message: 'Please enter the usage information for your project',
+          name: 'usage',
+        },
+        {
+          type: 'input',
+          message: 'Please enter the contribution guidelines for your project',
+          name: 'guidelines',
+        },
+        {
+          type: 'input',
+          message: 'Please enter the test instructions for your project',
+          name: 'testinst',
+        },
+            
+      ])
+
 ];
 
 // function to write README file
